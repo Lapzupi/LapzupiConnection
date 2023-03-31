ad41eb123d21caca6b886dbdede99a3a135d90ea
Omer Oreg <sarhatabaot1@gmail.com>
2023-03-31 16:27:42 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ dependencyResolutionManagement {
         create("libs") {
             version("flyway", "9.16.0")
             library("flyway-core", "org.flywaydb","flyway-core").versionRef("flyway")
-            library("flyway-core", "org.flywaydb","flyway-mysql").versionRef("flyway")
+            library("flyway-mysql", "org.flywaydb","flyway-mysql").versionRef("flyway")
         }
     }
 }
