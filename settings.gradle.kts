27a68eb6013434118b6078750684eaae8720a275
Omer Oreg <sarhatabaot1@gmail.com>
2023-03-31 16:18:31 +0300

    Added version catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "connection"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            version("flyway", "9.16.0")
+            library("flyway-core", "org.flywaydb","flyway-core").versionRef("flyway")
+            library("flyway-core", "org.flywaydb","flyway-mysql").versionRef("flyway")
+        }
+    }
+}
